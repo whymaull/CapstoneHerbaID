f841c45d121a6900fcc174d93b2f7cfcf9410ee3
deanaura <103201935+deanaura@users.noreply.github.com>
2023-12-19 22:39:47 +0700

    herbalController.js

--- Cloud Computing/src/controllers/herbalController.js
+++ Cloud Computing/src/controllers/herbalController.js
@@ -1,40 +1,59 @@
-const { v4: uuidv4 } = require("uuid");
-const { uploadBytes, getDownloadURL } = require("firebase/storage");
-const imageProcessing = require("../utils/imageProcessing");
-const { db, collection, addDoc } = require("../config/firebase");
+const { storage, uploadBytes, getDownloadURL, ref } = require("../config/firebase");
+const HerbalModel = require("../models/herbalModel");
+const HerbalService = require("../services/herbalService");
 
 // Fungsi untuk mengunggah gambar ke Firebase Storage
 const uploadImageToStorage = async (file) => {
   try {
-    const storageRef = storage.ref(); 
-    const fileName = `${uuidv4()}_${file.originalname}`;
-    const imageRef = storageRef.child(`images/${fileName}`); 
+    const storageRef = ref(storage, "image-identify");
+    const fileName = file.name;
+    const imageRef = ref(storageRef, fileName);
 
-    await uploadBytes(imageRef, file.buffer); 
+    await uploadBytes(imageRef, file);
 
-    const imageUrl = await getDownloadURL(imageRef); 
+    const imageUrl = await getDownloadURL(imageRef);
+    console.log("Image uploaded. URL:", imageUrl);
     return imageUrl;
   } catch (error) {
+    console.error("Error uploading image:", error);
     throw error;
   }
 };
 
-// Fungsi untuk mengidentifikasi herbal dari gambar
+// Fungsi untuk identifikasi herbal dari gambar yang diunggah
 exports.identifyHerbal = async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No image provided" });
     }
 
-    // Proses gambar untuk identifikasi herbal
     const imageUrl = await uploadImageToStorage(req.file);
-    const identifiedHerbal = await imageProcessing.identifyHerbal(imageUrl); 
 
-    // Simpan data herbal ke database
-    const herbalRef = collection(db, "herbals");
-    const newHerbalDoc = await addDoc(herbalRef, identifiedHerbal);
+    const identifiedHerbal = "Nama Herbal yang Teridentifikasi";
 
-    res.status(200).json(identifiedHerbal);
+    if (identifiedHerbal && typeof identifiedHerbal === "string") {
+      const herbalData = await HerbalModel.getHerbalByName(identifiedHerbal);
+
+      if (herbalData) {
+        const recipes = await HerbalService.getRecipesByHerbalId(herbalData.recipeIds);
+
+        res.status(200).json({
+          herbalData: {
+            herbalId: herbalData.herbalId,
+            name: herbalData.name,
+            imageURL: imageUrl,
+            about: herbalData.about,
+            benefits: herbalData.benefits,
+            recipeIds: herbalData.recipeIds,
+          },
+          recipes: recipes,
+        });
+      } else {
+        res.status(404).json({ message: "Herbal not found" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid identified herbal name" });
+    }
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
